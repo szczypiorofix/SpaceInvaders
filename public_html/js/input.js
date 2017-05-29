a43d4df542efad48edc6ab7b37ddbaca5816cce9
szczypiorofix <szczypiorofix@o2.pl>
2017-05-29 16:20:21 +0200

    Jest już w miarę ogarnięte.

---
+++ public_html/js/input.js
@@ -0,0 +1,49 @@
+var Input = function() {
+    
+    this.keyLeft = null;
+    this.keyRight = null;
+    this.keyUp = null;
+    this.keyDown = null;
+    
+    this.keyLeft = keyboard(37);
+    this.keyRight = keyboard(39);
+    this.keyUp = keyboard(38);
+    this.keyDown = keyboard(40);
+    
+    function keyboard(keyCode) {
+        var key = {};
+        key.code = keyCode;
+        key.isDown = false;
+        key.isUp = true;
+        key.press = undefined;
+        key.release = undefined;
+        //The `downHandler`
+        key.downHandler = function(event) {
+          if (event.keyCode === key.code) {
+            if (key.isUp && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+          }
+          event.preventDefault();
+        };
+
+        //The `upHandler`
+        key.upHandler = function(event) {
+          if (event.keyCode === key.code) {
+            if (key.isDown && key.release) key.release();
+            key.isDown = false;
+            key.isUp = true;
+          }
+          event.preventDefault();
+        };
+
+        //Attach event listeners
+        window.addEventListener(
+          "keydown", key.downHandler.bind(key), false
+        );
+        window.addEventListener(
+          "keyup", key.upHandler.bind(key), false
+        );
+        return key;
+      }
+};
--- public_html/js/mainscript.js
+++ public_html/js/mainscript.js
@@ -1,70 +1,90 @@
 
     var BG_COLOR = 0x111111;
-    var type = null;
-    var renderer = null;
-    var stage = null;
+    var ship = null;
+    var background = null;
+    var input = new Input();
+    var vx = 5, vy = 5;
     
     
     var Game = {
         screenWidth: 0,
         screenHeight: 0,
-        
+        renderer: null,
+        stage: null,
         initialize: function() {
 
-            var stage = new PIXI.Container(),
-            renderer = PIXI.autoDetectRenderer(256, 256);
-            document.body.appendChild(renderer.view);
-            renderer.backgroundColor = BG_COLOR;
-            renderer.autoResize = true;
-            renderer.view.style.position = "absolute";
-            renderer.view.style.display = "block";
-            renderer.resize(window.innerWidth, window.innerHeight);
-            document.body.appendChild(renderer.view);
-            stage = new PIXI.Container();
-            
-            PIXI.loader
-                .add("images/spaceship.png")
-                .load(setup);
-
-              //This `setup` function will run when the image has loaded
-                function setup() {
-
-                    var texture = PIXI.utils.TextureCache["images/tileset.png"];
-
-                      //Create a rectangle object that defines the position and
-                      //size of the sub-image you want to extract from the texture
-                      var rectangle = new PIXI.Rectangle(192, 128, 64, 64);
-
-                      //Tell the texture to use that rectangular section
-                      texture.frame = rectangle;
-
-                      //Create the sprite from the texture
-                      var rocket = new Sprite(texture);
-
-                      //Position the rocket sprite on the canvas
-                      rocket.x = 32;
-                      rocket.y = 32;
-
-                      //Add the rocket to the stage
-                      stage.addChild(rocket);
-
-                      //Render the stage   
-                      renderer.render(stage);
-                }
-
-            var message = new PIXI.Text(
-                "Hello Pixi!",
-                {fontFamily: "Tahoma", fontSize: 32, fill: "white"}
-            );
-
-            message.position.set(window.innerWidth /2, 96);
-            stage.addChild(message);
-              
-            renderer.render(stage);
+            this.stage = new PIXI.Container();
+            this.renderer = PIXI.autoDetectRenderer(256, 256);
+            document.body.appendChild(this.renderer.view);
+            this.renderer.backgroundColor = BG_COLOR;
+            this.renderer.autoResize = true;
+            this.renderer.view.style.position = "absolute";
+            this.renderer.view.style.display = "block";
+            this.renderer.resize(window.innerWidth, window.innerHeight);
+            this.screenWidth = this.renderer.width;
+            this.screenHeight = this.renderer.height;
+            document.body.appendChild(this.renderer.view);
+            this.stage = new PIXI.Container();
+            this.renderer.render(this.stage);
+        },
+        
+        addTextField: function(text, x, y, color, font, size) {
+            if (typeof color === 'undefined') color = 'white';
+            var t = new PIXI.Text(text, {fontFamily: font, fontSize: size, fill: color});
+            t.position.set(x, y);
+            this.stage.addChild(t);
         }
     };
     
+    function gameLoop() {
+         requestAnimationFrame(gameLoop);
+ 
+         if (input.keyLeft.isDown) {
+             ship.x -= vx;
+         }
+         
+         if (input.keyRight.isDown) {
+             ship.x += vx;
+         }
+         
+         if (input.keyUp.isDown) {
+             ship.y -= vy;
+         }
+         
+         if (input.keyDown.isDown) {
+             ship.y += vy;
+         }
+ 
+        Game.renderer.render(Game.stage);
+    }
+    
+    function loadSprites() {
+        
+        
+        PIXI.loader.add("images/spaceship.png").load(setup);
+
+        function setup() {
+
+            var texture = PIXI.utils.TextureCache["images/spaceship.png"];
+            var rectangle = new PIXI.Rectangle(0, 0, 32, 32);
+            texture.frame = rectangle;
+            ship = new PIXI.Sprite(texture);
+            ship.width = 64;
+            ship.height = 64;
+            ship.x = Game.screenWidth / 2;
+            ship.y = Game.screenHeight - ship.height - 20;
+            Game.stage.addChild(ship);
+            
+            Game.renderer.render(Game.stage);
+        }
+    }
     
     window.onload = function() {
+        
         Game.initialize();
+        loadSprites();
+        
+        Game.addTextField("SPACE INVADERS", (Game.screenWidth / 2) - 140, 50, 'red', 'Courier', 42);
+        
+        gameLoop();
     };
